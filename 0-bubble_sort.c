2fd2917345e3c85cda1bd0e25c392f806dafc12b
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-10-20 17:05:31 +0300

    betty styling

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,24 +9,25 @@
 
 void bubble_sort(int *array, size_t size)
 {
-  int t_swap;
-  size_t i, j;
+    int t_swap;
+    size_t i, j;
 
-  if (!array || !size)
-    return;
-  i = 0;
-  while (i < size)
-  {
-    for (j = 0; j < size; j++)
+    if (!array || !size)
+        return;
+
+    i = 0;
+    while (i < size)
     {
-      if (array[j] > array[j + 1])
-      {
-        t_swap = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = t_swap;
-        print_array(array, size);
-      }
+        for (j = 0; j < size; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                t_swap = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = t_swap;
+                print_array(array, size);
+            }
+        }
+        i++;
     }
-    i++;
-  }
 }
