4b34c28556ae11ec0402f51ea4ae0515b6a14c50
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-10-21 01:55:15 +0300

    1-insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * node_sw - function that swaps two nodes
+ * @left: left node
+ * @right: right node
+*/
+void node_sw(listint_t **left, listint_t **right)
+{
+(*left)->next = (*right)->next;
+(*right)->next = (*left)->prev;
+if ((*right)->next)
+(*right)->next->prev = (*left);
+if ((*left)->prev)
+(*left)->prev->next = (*right);
+(*right)->next = (*left);
+(*left)->prev = (*right);
+}
+
+/**
+ * insertion_sort_list - function sorts lists in an ascending order
+ * @list: head of list to be sorted
+*/
+void insertion_sort_list(listint_t **list)
+{
+listint_t *temp = NULL, *curr = NULL, *prev = NULL;
+if (list && *list && (*list)->next)
+{
+temp = (*list)->next;
+while (temp)
+{
+curr = temp;
+prev = temp->prev;
+while (prev && curr->n < prev->n)
+{
+swap_nodes(&prev, &curr);
+if (prev == *list) /* if curr now list head */
+*list = curr;
+print_list(*list);
+prev = curr->prev;
+}
+temp = temp->next;
+}
+}
+}
