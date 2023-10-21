c429a3d21e6e0b75a455188840945a71dfd29df8
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-10-22 01:17:18 +0300

    aa

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -60,7 +60,7 @@ my_quickksurt(array, size, pvt + 1, high);
 */
 void quick_sort(int *array, size_t size)
 {
-if (array && size > 1)
+if (array == NULL|| size == 0)
 return;
 my_quickksurt(array, size, 0, size - 1);
 }
