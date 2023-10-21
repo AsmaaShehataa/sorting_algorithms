c3ed569b13796066423843c637bd6ee7e96e8d44
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-10-21 22:55:18 +0300

    edit in file of insertion

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -17,19 +17,19 @@ return;
 }
 for (i = 0; i < size - 1; i++)
 {
-for (j = size - 1, k = i + 1; j > i; j--)
-{
-if (array[j] < array[k])
-{
-k = j;
-}
-}
-}
-if (array[i] > array[k])
-{
-tmp_sw = array[i];
-array[i] = array[k];
-array[k] = tmp_sw;
-print_array(array, size);
+  for (j = size - 1, k = i + 1; j > i; j--)
+  {
+    if (array[j] < array[k])
+    {
+      k = j;
+    }
+  }
+  if (array[i] > array[k])
+  {
+    tmp_sw = array[i];
+    array[i] = array[k];
+    array[k] = tmp_sw;
+    print_array(array, size);
+  }
 }
 }
